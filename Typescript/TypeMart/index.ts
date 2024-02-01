d9a863fde26c4e42916d14412211151f44cfbc35
smolbebby <125963415+smolbebby@users.noreply.github.com>
2024-02-01 21:57:24 +1000

    Create index.ts

---
+++ Typescript/TypeMart/index.ts
@@ -0,0 +1,42 @@
+import products from './products';
+
+const productName: string = 'fanny pack';
+const product = products.filter(product => product.name === productName)[0];
+
+
+if (product.preOrder === 'true'){
+  console.log('We will send you a message when your product is on the way.');
+}
+
+let shipping: number;
+let taxPercent: number;
+let taxTotal: number;
+let total:number;
+
+let shippingAddress: string = '32 Wallaby Way, Sydney, New South Wales';
+if (price => 25){
+  shipping = 0;
+  console.log('We are providing free shipping for this product.');
+} else {
+  shipping = 5;
+}
+
+if (shippingAddress.match('New York')){
+  taxPercent = 0.1;
+} else {
+  taxPercent = 0.05;
+}
+
+taxTotal = Number(product.price) * taxPercent;
+total = Number(product.price) + taxTotal + shipping;
+
+console.log(`
+Product:  ${product.name}
+Address:  ${shippingAddress}
+Price:    $${product.price}
+Tax:      $${taxTotal.toFixed(2)}
+Shipping: $${shipping.toFixed(2)}
+Total:    $${total.toFixed(2)}
+`);
+
+
